74a634aff41c0b4ab421475554ae4cc0966a5e85
gabrielmg7 <201902661559@alunos.estacio.br>
2024-01-05 18:38:04 -0300

    "no-unused-vars": "off"

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import MenuBar from '../../components/MenuBar'
 
 export const Home = () => {
     return (
-        <div>Home
-
-        </div>
+        <MenuBar/>
     )
 }
