3e1b495168cdce4f82f22ff753f1ebe0a02d282f
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2024-02-07 13:24:24 -0300

    versão estável -> Cliente/Admin passam a ser User

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { UserProvider } from './Contexts/UserContext.tsx';
-import { AdminProvider } from './Contexts/AdminContext.tsx';
 import { ThemeProviderWrapper } from './Themes/ThemeProviderWrapper.tsx';
 
 
@@ -13,9 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProviderWrapper>
       <Router>
         <UserProvider>
-          <AdminProvider>
             <App />
-          </AdminProvider>
         </UserProvider>
       </Router>
     </ThemeProviderWrapper>
