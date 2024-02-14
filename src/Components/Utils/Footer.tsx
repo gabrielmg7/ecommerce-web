2ab191e11b2195b3e2d5955b43e10dfbdf3f0148
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2024-02-14 20:46:46 -0300

    incluindo páginas de checkout, carrinho e pagamento

--- src/Components/Utils/Footer.tsx
+++ src/Components/Utils/Footer.tsx
@@ -4,60 +4,65 @@ import AnimatedGrid from './AnimatedGrid';
 
 const Footer = () => {
     return (
-        <Box py={3} justifyContent={'space-around'}>
+        <Box>
+            <Box mt={1} mb={1}>
+                <Divider orientation="horizontal" />
+            </Box>
+            
             <AnimatedGrid>
-                <Box mt={2} mb={2}>
-                    <Divider orientation="horizontal" />
-                </Box>
-                <Grid container justifyContent="center" alignItems="center" spacing={50}>
-                    <Grid item>
+
+                <Grid container direction="row" justifyContent="space-around" alignItems="center" >
+
+                    <Grid item direction="column" justifyContent="center" alignItems={'center'}>
                         <Logo logo='Logo1' width='150px' height='150px' />
                     </Grid>
 
                     <Grid item>
-                        <Grid container direction="column">
-                            <Grid item>
-                                <Typography variant="subtitle2">
-                                    <Link href="/" color="text.primary" underline="hover">
-                                        Home
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle2">
-                                    <Link href="/listar-produtos" color="text.primary" underline="hover">
-                                        Produtos
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle2">
-                                    <Link href="/carrinho" color="text.primary" underline="hover">
-                                        Meu Carrinho
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle2">
-                                    <Link href="#" color="text.primary" underline="hover">
-                                        Contato
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle2">
-                                    <Link href="/sobre" color="text.primary" underline="hover">
-                                        Sobre Nós
-                                    </Link>
-                                </Typography>
-                            </Grid>
+
+                        <Grid item direction="column" justifyContent="center" alignItems={'center'}>
+                            <Typography variant="subtitle2">
+                                <Link href="/" color="text.primary" underline="hover">
+                                    Home
+                                </Link>
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant="subtitle2">
+                                <Link href="/listar-produtos" color="text.primary" underline="hover">
+                                    Produtos
+                                </Link>
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant="subtitle2">
+                                <Link href="/carrinho" color="text.primary" underline="hover">
+                                    Meu Carrinho
+                                </Link>
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant="subtitle2">
+                                <Link href="#" color="text.primary" underline="hover">
+                                    Contato
+                                </Link>
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="subtitle2">
+                                <Link href="/sobre" color="text.primary" underline="hover">
+                                    Sobre Nós
+                                </Link>
+                            </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Box mt={2} mb={2}>
-                    <Divider orientation="horizontal" />
-                </Box>
             </AnimatedGrid>
+            <Box mt={1} mb={1}>
+                <Divider orientation="horizontal" />
+            </Box>
         </Box>
     );
 };
