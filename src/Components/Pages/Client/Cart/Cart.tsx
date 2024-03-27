2098c57753eadc3a59baae2bae8fda6cd11e521e
gabrielmg7 <202302332137@alunos.estacio.br>
2024-03-27 18:54:39 -0300

    criando pagina de carrinho

--- src/Components/Pages/Client/Cart/Cart.tsx
+++ src/Components/Pages/Client/Cart/Cart.tsx
@@ -8,29 +8,49 @@ import FindInPageTwoToneIcon from '@mui/icons-material/FindInPageTwoTone';
 const Cart = () => {
   return (
     <ClientLayout>
-      <Grid container direction={"row"} justifyContent={"space-evenly"} alignItems={"center"}>
-        {/* ===================================== Coluna 1 ===================================== */}
-        <Grid item container direction={"column"} sm={3} md={5} lg={6} xl={6} bgcolor={"background.paper"}>
-          {/* ===================================== Endereço de Entrega ===================================== */}
-          <Grid container direction={"row"} alignItems={"center"} justifyContent={"flex-start"} columnGap={1}>
-            <MarkunreadMailboxTwoToneIcon color='action' />
-            <Typography color={"text.primary"} variant="h5">Endereço de Entrega</Typography>
+      <Grid container direction={"row"} justifyContent={"space-evenly"} alignItems={"flex-start"}>
+        {/* Coluna 1 ===================================== */}
+        <Grid container direction={"column"} justifyContent="space-evenly" gap={2} alignItems="center" sm={3} md={5} lg={6} xl={6}>
+          {/* Endereço de Entrega ===================================== */}
+          <Grid item container bgcolor={"background.paper"}>
+            {/* Título da Seção ===================================== */}
+            <Grid item container 
+            direction={"row"}
+            alignItems={"center"} 
+            justifyContent={"flex-start"} 
+            columnGap={1}
+            >
+              <MarkunreadMailboxTwoToneIcon color='action' />
+              <Typography color={"text.primary"} variant="h5">Endereço de Entrega</Typography>
+            </Grid>
+            {/* Conteúdo da Seção ===================================== */}
+            <Grid>
+
+            </Grid>
           </Grid>
-          {/* ===================================== Produtos ===================================== */}
-          <Grid>
-            <Grid item direction={"column"} bgcolor={"background.paper"}>
-              <Grid container direction={"row"} alignItems={"center"} justifyContent={"flex-start"} columnGap={1}>
-                <LocalMallTwoToneIcon color='action' />
-                <Typography color={"text.primary"} variant="h5">Produtos</Typography>
-              </Grid>
+          {/* Produtos ===================================== */}
+          <Grid item container bgcolor={"background.paper"}>
+            {/* Título da Seção ===================================== */}
+            <Grid item container
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"flex-start"}
+              columnGap={1}
+            >
+              <LocalMallTwoToneIcon color='action' />
+              <Typography color={"text.primary"} variant="h5">Produtos</Typography>
+            </Grid>
+            {/* Conteúdo da Seção ===================================== */}
+            <Grid>
+
             </Grid>
           </Grid>
         </Grid>
 
-        {/* ===================================== Resumo do Pedido ===================================== */}
+        {/* Resumo do Pedido ===================================== */}
         <Grid item direction={"column"} bgcolor={"background.paper"} sm={1} md={2} lg={4} xl={5}>
-          <Grid container direction={"row"} alignItems={"center"} justifyContent={"flex-start"} columnGap={1}> 
-            <FindInPageTwoToneIcon color='action'/>
+          <Grid container direction={"row"} alignItems={"center"} justifyContent={"flex-start"} columnGap={1}>
+            <FindInPageTwoToneIcon color='action' />
             <Typography color={"text.primary"} variant="h5">Resumo</Typography>
           </Grid>
         </Grid>
