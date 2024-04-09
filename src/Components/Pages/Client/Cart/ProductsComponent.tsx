a40098a3e708b314a8d117709c7932ea91b3af7e
gabrielmg7 <202302332137@alunos.estacio.br>
2024-04-09 00:31:53 -0300

    Criando componente de Produtos da Página de Carrinho

---
+++ src/Components/Pages/Client/Cart/ProductsComponent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Avatar, Grid, Typography } from '@mui/material';
+import { useCarrinhoContext } from '../../../../contexts/CartContext';
+import { useProduct } from '../../../../contexts/ProductContext';
+
+const ProductsComponent = () => {
+    const { carrinho } = useCarrinhoContext();
+    const { produtos } = useProduct();
+
+    return (
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+            {carrinho?.itens.map(item => {
+                const produto = produtos?.find(prod => prod.id === item.idProduto);
+                if (!produto) return null; // Se o produto não for encontrado, retornar null
+
+                return (
+                    <Grid key={item.id} container direction="row" bgcolor="background.card" alignItems="center">
+                        <Grid item>
+                            <Avatar sx={{ width: 100, height: 100 }} variant="rounded" />
+                        </Grid>
+                        <Grid item marginLeft={1}>
+                            <Typography variant="body1" color="text.primary">
+                                {produto.nome} {/* Exibindo o nome do produto */}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Quantidade: {item.quantidade} {/* Exibindo a quantidade */}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Preço Unitário: {item.valorUnitario} {/* Exibindo o preço unitário */}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                );
+            })}
+        </Grid>
+    );
+};
+
+export default ProductsComponent;
