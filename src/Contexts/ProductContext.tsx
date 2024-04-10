62527fd997fe16ce195013486c2b6ec9c367b189
gabrielmg7 <202302332137@alunos.estacio.br>
2024-04-10 16:19:37 -0300

    Criando um Context para manipular os produtos

--- src/Contexts/ProductContext.tsx
+++ src/Contexts/ProductContext.tsx
@@ -1,33 +1,40 @@
-// ProductContext.tsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { getAllProducts } from '../services/fakeAPI/fakeApiProductService';
-import { IProduto } from '../types/restAPI/IProduto';
+import { IProduct } from '../types/FakeAPI/type';
 
 interface ProductContextProps {
-    produtos: IProduto[] | null;
+    produtos: IProduct[] | null;
     carregarProdutos: () => Promise<void>;
+    atualizarProdutos: (novosProdutos: IProduct[]) => void; 
 }
 
 const ProductContext = createContext<ProductContextProps | undefined>(undefined);
 
 export const ProductProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [produtos, setProdutos] = useState<IProduto[] | null>(null);
-
+    const [produtos, setProdutos] = useState<IProduct[] | null>(null);
+    
     const carregarProdutos = async () => {
         try {
-            const produtosCarregados = await getAllProducts();
+            const produtosCarregados: IProduct[] = await getAllProducts();
             setProdutos(produtosCarregados);
+            console.log("Produtos Carregados: ", produtosCarregados); 
         } catch (error) {
             console.error('Erro ao carregar produtos:', error);
         }
     };
-
+    
+    
     useEffect(() => {
         carregarProdutos();
     }, []); 
 
+    
+    const atualizarProdutos = (novosProdutos: IProduct[]) => {
+        setProdutos(novosProdutos);
+    };
+
     return (
-        <ProductContext.Provider value={{ produtos, carregarProdutos }}>
+        <ProductContext.Provider value={{ produtos, carregarProdutos, atualizarProdutos }}>
             {children}
         </ProductContext.Provider>
     );
